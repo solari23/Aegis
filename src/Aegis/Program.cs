223a69baccdf9d9e6223f3456cc5990d0227142d
Alexandre Kerametlian <akerametlian@outlook.com>
2019-01-15 21:57:28 -0800

    Create VS solution and project files

---
+++ src/Aegis/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Aegis
+{
+    using System;
+
+    /// <summary>
+    /// Contains the main entry point for the Aegis CLI.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The entry point for the Aegis CLI.
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
