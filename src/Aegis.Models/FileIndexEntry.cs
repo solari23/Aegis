58f3ff50c4ecc9f7925660fdc4f55f3f3488b460
Alexandre Kerametlian <akerametlian@live.com>
2020-03-10 17:01:00 -0700

    Create 'models' assembly with JSON-based archive data structures

---
+++ src/Aegis.Models/FileIndexEntry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aegis.Models
+{
+    /// <summary>
+    /// Metadata about a file stored in the secure archive.
+    /// </summary>
+    public class FileIndexEntry
+    {
+        /// <summary>
+        /// A unique identifier for the file within the archive.
+        /// </summary>
+        public Guid FileId { get; set; }
+
+        /// <summary>
+        /// A virtual file name. These must be unique within the archive.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// When the file was added to the archive.
+        /// </summary>
+        public DateTimeOffset AddedTime { get; set; }
+
+        /// <summary>
+        /// When the archived file was last modified.
+        /// </summary>
+        public DateTimeOffset LastModifiedTime { get; set; }
+    }
+}
---
+++ src/Aegis.Models/SecureArchiveMetadata.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+using Aegis.Models.JsonConverters;
+
+namespace Aegis.Models
+{
+    /// <summary>
+    /// The core data structure that maintains metadata about the archive, including user key authorizations
+    /// and an index of the encrypted documents stored in the archive.
+    /// </summary>
+    public class SecureArchiveMetadata
+    {
+        /// <summary>
+        /// A unique identifier for the archive.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The security settings for the archive.
+        /// </summary>
+        public SecuritySettings SecuritySettings { get; set; }
+
+        /// <summary>
+        /// When the archive was created.
+        /// </summary>
+        public DateTimeOffset CreateTime { get; set; }
+
+        /// <summary>
+        /// When the archive was last modified.
+        /// </summary>
+        public DateTimeOffset LastModifiedTime { get; set; }
+
+        /// <summary>
+        /// The salt used when deriving keys.
+        /// </summary>
+        [JsonConverter(typeof(JsonByteListBase64Converter))]
+        [SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "Required for serialization framework.")]
+        public List<byte> KeyDerivationSalt { get; set; }
+
+        /// <summary>
+        /// The archive ID encrypted with the archive key. This field is used to
+        /// test if the presented user key is authorized to unlock the archive.
+        /// </summary>
+        public EncryptedPacket AuthCanary { get; set; }
+
+        /// <summary>
+        /// The list of keys that are authorized to unlock the archive.
+        /// </summary>
+        [SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "Required for serialization framework.")]
+        public List<UserKeyAuthorization> UserKeyAuthorizations { get; set; }
+
+        /// <summary>
+        /// The encrypted file index.
+        /// </summary>
+        /// <remarks>
+        /// The encrypted data is JSON formatted list of <see cref="FileIndexEntry"/> objects.
+        /// </remarks>
+        public EncryptedPacket EncryptedFileIndex { get; set; }
+    }
+}
