def48ea4b4daf1b96a64553309545934dd97c76d
Alexandre Kerametlian <akerametlian@outlook.com>
2022-02-26 00:29:23 -0800

    [Tests] Add basic test to verify archive insert and extraction

---
+++ src/Aegis.Test/ArchiveTestHelpers.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+
+using Aegis.Core;
+using Aegis.Core.CredentialsInterface;
+using Aegis.Models;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aegis.Test;
+
+/// <summary>
+/// Helpers for test cases that deal with Aegis archives.
+/// </summary>
+public static class ArchiveTestHelpers
+{
+    public static class SampleFiles
+    {
+        /// <summary>
+        /// The path to a basic text sample file for use in tests.
+        /// </summary>
+        public const string SimpleTextFilePath = @"SampleFiles\SimpleTextFile.txt";
+    }
+
+    /// <summary>
+    /// The default password for test archives.
+    /// </summary>
+    public const string DefaultPassword = "P@$$w3rd";
+
+    /// <summary>
+    /// The default file name for test archives.
+    /// </summary>
+    public const string DefaultTestArchiveName = "TestArchive.ags";
+
+    /// <summary>
+    /// Gets the <see cref="DefaultPassword"/> as a <see cref="RawUserSecret"/>.
+    /// </summary>
+    public static RawUserSecret DefaultPasswordUserSecret =>
+        new RawUserSecret(Encoding.UTF8.GetBytes(DefaultPassword));
+
+    public static SecureArchiveFileSettings GetTestArchiveFileSettings(string workingDirectory, string archiveName = null) =>
+        new SecureArchiveFileSettings(
+            archiveFilePath: Path.Combine(workingDirectory, archiveName ?? DefaultTestArchiveName),
+            tempDirectory: workingDirectory);
+
+    /// <summary>
+    /// Creates a new Aegis archive for testing.
+    /// The password for the archive will be <see cref="DefaultPassword"/>.
+    /// </summary>
+    /// <param name="workingDirectory">The working directory where the archive will be created.</param>
+    /// <param name="archiveName">The name of the archive (Optional. Default: <see cref="DefaultTestArchiveName"/>.)</param>
+    /// <returns>The new archive, which will be open and unlocked.</returns>
+    public static AegisArchive CreateNewEmptyArchive(string workingDirectory, string archiveName = null) =>
+        AegisArchive.CreateNew(
+            GetTestArchiveFileSettings(workingDirectory, archiveName),
+            new SecureArchiveCreationParameters(
+                new UserKeyAuthorizationParameters(DefaultPasswordUserSecret)
+                {
+                    FriendlyName = "TestPassword",
+                    SecretMetadata = new PasswordSecretMetadata(),
+                }));
+
+    /// <summary>
+    /// Compares a test file to a reference file and asserts that they match.
+    /// </summary>
+    /// <param name="referenceFilePath">The path to the reference file.</param>
+    /// <param name="testFilePath">The path to the test file.</param>
+    /// <param name="deleteTestFile">Whether to delete the test file from disk when done checking.</param>
+    public static void CompareFileToReference(
+        string referenceFilePath,
+        string testFilePath,
+        bool deleteTestFile = false)
+    {
+        Assert.IsTrue(
+            File.Exists(testFilePath),
+            $"The test file at path '{testFilePath}' is missing!");
+
+        var referenceFileBytes = File.ReadAllBytes(referenceFilePath);
+        var testFileBytes = File.ReadAllBytes(testFilePath);
+
+        Assert.AreEqual(
+            referenceFileBytes.Length,
+            testFileBytes.Length, $"The test file at path '{testFilePath}' does not have the expected size!");
+
+        for (int i = 0; i < testFileBytes.Length; i++)
+        {
+            if (referenceFileBytes[i] != testFileBytes[i])
+            {
+                Assert.Fail($"The test file at path '{testFilePath}' differs in content from the reference file!");
+            }
+        }
+
+        if (deleteTestFile)
+        {
+            File.Delete(testFilePath);
+        }
+    }
+}
---
+++ src/Aegis.Test/Core/AegisArchiveTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Aegis.Core;
+using Aegis.Core.FileSystem;
+
+namespace Aegis.Test.Core;
+
+[TestClass]
+public class AegisArchiveTests
+{
+    public TestContext TestContext { get; set; }
+
+    private string WorkingDirectory { get; set; }
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        this.WorkingDirectory = Path.Combine(
+            this.TestContext.TestRunDirectory,
+            $"{this.TestContext.TestName}_Work");
+        Directory.CreateDirectory(this.WorkingDirectory);
+    }
+
+    [TestMethod]
+    [Description("Tests adding and extracting a single file.")]
+    public void TestArchive_BasicScenario()
+    {
+        // 1. Create a new archive
+        // 2. Add a file to it
+        // 3. Extract the file, ensure it is identical to the original
+        // 4. Close the archive
+        // 5. Reopen the archive and decrypt
+        // 6. Extract the file again, ensure it is identical to the original
+
+        AegisFileInfo file = null;
+
+        using (var archive = ArchiveTestHelpers.CreateNewEmptyArchive(this.WorkingDirectory))
+        {
+            // The achive should be created in unlocked state.
+            Assert.IsFalse(archive.IsLocked);
+
+            file = archive.PutFile(
+                new AegisVirtualFilePath("text.txt"),
+                File.OpenRead(ArchiveTestHelpers.SampleFiles.SimpleTextFilePath));
+
+            var extractPath1 = Path.Combine(this.WorkingDirectory, "Text_out1.txt");
+            using (var extractStream = File.OpenWrite(extractPath1))
+            {
+                archive.ExtractFile(file, extractStream);
+            }
+
+            ArchiveTestHelpers.CompareFileToReference(
+                referenceFilePath: ArchiveTestHelpers.SampleFiles.SimpleTextFilePath,
+                testFilePath: extractPath1,
+                deleteTestFile: true);
+        }
+
+        using var reopenedArchive = AegisArchive.Load(
+            ArchiveTestHelpers.GetTestArchiveFileSettings(this.WorkingDirectory));
+
+        // The archive is open but still locked. Make sure the flags are set accordingly.
+        Assert.IsTrue(reopenedArchive.IsLocked);
+        Assert.ThrowsException<ArchiveLockedException>(() => reopenedArchive.GetFileInfo(file.FileId));
+
+        reopenedArchive.Unlock(ArchiveTestHelpers.DefaultPasswordUserSecret);
+        Assert.IsFalse(reopenedArchive.IsLocked);
+
+        var extractPath2 = Path.Combine(this.WorkingDirectory, "Text_out2.txt");
+        using (var extractStream = File.OpenWrite(extractPath2))
+        {
+            reopenedArchive.ExtractFile(file, extractStream);
+        }
+
+        ArchiveTestHelpers.CompareFileToReference(
+            referenceFilePath: ArchiveTestHelpers.SampleFiles.SimpleTextFilePath,
+            testFilePath: extractPath2,
+            deleteTestFile: true);
+    }
+
+    [TestMethod]
+    public void TestArchive_DeleteFile()
+    {
+        // TODO
+    }
+}
