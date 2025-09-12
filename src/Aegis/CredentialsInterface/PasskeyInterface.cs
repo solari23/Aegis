e6c320c9f1a343518004224bf0db391d67bdb209
Alexandre Kerametlian <akerametlian@outlook.com>
2025-09-12 15:19:40 -0700

    Add passkey secret support to Aegis

---
+++ src/Aegis/CredentialsInterface/PasskeyInterface.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Immutable;
+
+using Aegis.Core;
+using Aegis.Core.CredentialsInterface;
+using Aegis.Core.Crypto;
+using Aegis.Models;
+using Aegis.Passkeys;
+
+namespace Aegis.CredentialsInterface;
+
+/// <summary>
+/// Implementation of <see cref="IUserSecretEntryInterface"/> for interacting with certificates from the command line.
+/// </summary>
+public class PasskeyInterface : IUserSecretEntryInterface
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PasskeyInterface"/> class.
+    /// </summary>
+    /// <param name="ioStreamSet">The IO streams.</param>
+    public PasskeyInterface(IOStreamSet ioStreamSet)
+    {
+        ArgCheck.NotNull(ioStreamSet);
+
+        this.IO = ioStreamSet;
+        this.PasskeyManager = new PasskeyManager();
+    }
+
+    /// <inheritdoc />
+    public SecretKind ProvidedSecretKind => SecretKind.PasskeyHmacSecret;
+
+    /// <summary>
+    /// Gets the IO streams.
+    /// </summary>
+    private IOStreamSet IO { get; }
+
+    /// <summary>
+    /// The manager class to use for interacting with passkeys.
+    /// </summary>
+    private PasskeyManager PasskeyManager { get; }
+
+    /// <inheritdoc />
+    public bool CanProvideSecret(SecretMetadata secretMetadata) => this.PasskeyManager.IsHmacSecretSupported();
+
+    /// <inheritdoc />
+    public UserKeyAuthorizationParameters GetNewKeyAuthorizationParameters()
+    {
+        // TODO: Implement PasskeyPickerInterface.GetNewKeyAuthorizationParameters
+        throw new NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public RawUserSecret GetUserSecret(ImmutableArray<SecretMetadata> possibleSecretMetadata)
+    {
+        // TODO: Implement PasskeyPickerInterface.GetUserSecret
+        throw new NotImplementedException();
+    }
+}
